2735380c9bb6e2193ef27ca3d719e5ff738c59db
lakiiii <luckymaharjan2007@gmail.com>
2025-11-21 14:13:57 +0545

    finidhed work

---
+++ LiteralPractice.java
@@ -0,0 +1,27 @@
+
+/**
+ * Write a description of class LiteralPractice here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class LiteralPractice
+{
+    public static void main (String[] args)
+    {
+        long ln= 456789034567890l;
+        float fn= 4567890.45678f;
+        char ch ='\u00A9';
+        
+        System.out.println("the long number:"+ln);
+        System.out.println("the float number :"+fn);
+        System.out.println(ch);
+        
+    
+    
+    
+    
+    }  // 
+        
+        
+}

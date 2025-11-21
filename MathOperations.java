dc8ecf9a6ae23f98af6b6cdf46a3855410c7ca9f
lakiiii <luckymaharjan2007@gmail.com>
2025-11-21 13:16:29 +0545

    workshop 3

--- MathOperations.java
+++ MathOperations.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 /**
  * Write a description of class MathOperations here.
@@ -8,4 +9,27 @@
 public class MathOperations
 {
     public static void main (String[] args)
+    {
+        Scanner scan = new Scanner (System.in);
+        System.out.println("enter a number");
+        int a = scan.nextInt();
+        System.out.println("enter second number");
+        
+        //arthemetic 
+        int b = scan.nextInt();
+        int sum = a=b ;
+        System.out.println("the answer is :"+sum);
+        int pre_incriment= ++a;
+        int post_incriement= a++;
+        System.out.println(pre_incriment);
+        System.out.println(post_incriement);
+        
+        
+        
+        
+        
+                
+        
+        
+    }
 }
